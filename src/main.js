bd1bba05d818a43a9ab770eabaf6ae98ec0a2542
julian <julianMBP>
2019-12-25 23:42:29 +0100

    - added pwa support and service worker

--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ import Game from './components/Game'
 import Rules from './components/Rules'
 import Toast from "vue-toastification";
 import "vue-toastification/dist/index.css";
+import './registerServiceWorker'
 
 
 
